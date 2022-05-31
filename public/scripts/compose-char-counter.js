d1d620785d25789abf84f595bba73c529dc3081a
Aidan Antony <aidanantony65@gmail.com>
2022-05-30 21:12:02 -0600

    Add functionality to counter along with change in color for text past limit

---
+++ public/scripts/compose-char-counter.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("#tweet-text").on('input', function() {
+     $(".counter").text(140 - $(this).val().length);
+     let max = 140
+     let charsleft =  max - $(this).val().length
+     if (charsleft < 0) {
+       $(".counter").css({"color": "red"})
+     } else {
+       $(".counter").css({"color": "black"})
+     }
+  });
+});
